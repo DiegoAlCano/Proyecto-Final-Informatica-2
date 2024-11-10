cfd9fac477c50fc98982a68a4573889c9f603e69
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-11-10 03:10:20 -0500

    Update enemigo.cpp
    Se tiene el primer enemigo del juego. Este spawnea en un lado aleatorio de la pantalla y se mueve hacia el otro lado de la pantalla. Se mueve fluidamente con sus sprites.

--- enemigo.cpp
+++ enemigo.cpp
@@ -1,4 +1,70 @@
-#include "enemigo.h"
-#include "personaje.h"
+#include "Enemigo.h"
+#include <QGraphicsScene>
+#include "QDebug"
 
-enemigo::enemigo() {}
+Enemigo::Enemigo(bool lado)
+{
+    Enemigo_hojaSprites.load(":/Recursos/Enemigo1.png");
+    Enemigo_sprite = Enemigo_hojaSprites.copy(Enemigo_SpriteX,Enemigo_SpriteY,Enemigo_spriteAncho,Enemigo_spriteAlto);
+    setPixmap(Enemigo_sprite);
+
+    if (lado) {
+        setPos(0,560);
+        velocidad = 5;
+    } else {
+        setPos(540,560);
+        velocidad = -5;
+    }
+
+    // Temporizador para mover al enemigo
+    moveTimer = new QTimer(this);
+    connect(moveTimer, &QTimer::timeout, this, &Enemigo::moverHaciaHeroe);
+    moveTimer->start(50);
+
+}
+
+void Enemigo::moverHaciaHeroe() {
+
+    setPos(x()+velocidad,560);
+
+
+    if(velocidad==5){
+        Enemigo_confSprite(0);
+    }
+    else{
+        Enemigo_confSprite(50);
+    }
+
+
+    /*
+    // Verificar colisión con el héroe
+    QList<QGraphicsItem *> collidingItemsList = collidingItems();
+    for (QGraphicsItem *item : collidingItemsList) {
+        if (dynamic_cast<Heroe *>(item)) {
+            // Si el enemigo golpea al héroe, se elimina el enemigo
+            scene()->removeItem(this);
+            delete this;
+            return;
+        }
+    }
+    */
+
+    // Si el enemigo sale de los límites de la escena, se elimina
+    if (x() < -50|| x() > 570) {  // Ajusta estos límites según el ancho de tu escena
+        scene()->removeItem(this);
+        delete this;
+    }
+
+}
+
+void Enemigo::Enemigo_confSprite(int direccion)
+{
+    Enemigo_SpriteY = direccion;
+    Enemigo_SpriteX = Enemigo_spriteAncho*Enemigo_contador;
+    Enemigo_sprite = Enemigo_hojaSprites.copy(Enemigo_SpriteX,Enemigo_SpriteY,Enemigo_spriteAncho,Enemigo_spriteAlto);
+    setPixmap(Enemigo_sprite);
+    Enemigo_contador++;
+    if(Enemigo_contador==7){
+        Enemigo_contador = 0;
+    }
+}
