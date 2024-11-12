8cd9523437be49b9dc8cf12ff7b0a486f5f6b2c6
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-11-11 21:28:11 -0500

    Update Enemigo.h
    Se hacen los cambios correspondientes dada la herencia de la clase Personaje. Se eliminan los atributos y métodos ya heredados

--- Enemigo.h
+++ Enemigo.h
@@ -2,26 +2,16 @@
 #define ENEMIGO_H
 
 #include "Heroe.h"
-#include <QObject>
-#include <QGraphicsPixmapItem>
+#include "Personaje.h"
 #include <QTimer>
 
-class Enemigo : public QObject, public QGraphicsPixmapItem
+class Enemigo : public Personaje
 {
     Q_OBJECT
 public:
-    Enemigo(bool lado);
+    Enemigo(bool lado,int _SpriteX, int _SpriteY, int _spriteAncho, int _spriteAlto, std::string rutaSprite);
     void moverHaciaHeroe();
-    void Enemigo_confSprite(int direccion);
 private:
-
-    int Enemigo_SpriteX = 0;
-    int Enemigo_SpriteY = 0;
-    int Enemigo_spriteAncho = 100;
-    int Enemigo_spriteAlto = 50;
-    QPixmap Enemigo_hojaSprites;
-    QPixmap Enemigo_sprite;
-    int Enemigo_contador;
 
     QTimer *moveTimer;
     int velocidad;
