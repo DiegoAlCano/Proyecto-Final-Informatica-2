90531b497c0aedb6984819392628953038f8293b
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-11-11 21:30:24 -0500

    Update Heroe.h
    Se hacen los cambios correspondientes dada la herencia de la clase Personaje. Se eliminan atributos y métodos ya heredados.

--- Heroe.h
+++ Heroe.h
@@ -1,31 +1,21 @@
 #ifndef HEROE_H
 #define HEROE_H
 
-#include <QObject>
-#include "QGraphicsPixmapItem"
-#include "QPixmap"
+#include "Personaje.h"
 #include "QKeyEvent"
 #include "mainwindow.h"
 
-class Heroe : public QObject, public QGraphicsPixmapItem
+class Heroe : public Personaje
 {
     Q_OBJECT
 public:
-    Heroe();
+    Heroe(int _SpriteX, int _SpriteY, int _spriteAncho, int _spriteAlto,std::string rutaSprite);
     void keyPressEvent(QKeyEvent *event);
     void movimiento(int dx, int dy);
-    void confSprite(int direccion);
 private:
+
     qreal x = 0;
     qreal y = 450;
-
-    int SpriteX = 0;
-    int SpriteY = 0;
-    int spriteAncho = 150;
-    int spriteAlto = 160;
-    QPixmap hojaSprites;
-    QPixmap sprite;
-    int contador;
 };
 
 #endif // HEROE_H
