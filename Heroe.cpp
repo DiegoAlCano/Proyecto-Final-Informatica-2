7c161a211953bb89f9faa9127574721326ed3fa2
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-11-11 21:37:19 -0500

    Update Heroe.cpp
    Se modifica el constructor de la clase Heroe y se elimina el método confSprite.

--- Heroe.cpp
+++ Heroe.cpp
@@ -3,9 +3,10 @@
 #include "QDebug"
 #include "mainwindow.h"
 
-Heroe::Heroe()
+Heroe::Heroe(int _SpriteX, int _SpriteY, int _spriteAncho, int _spriteAlto, std::string rutaSprite)
+    :Personaje(_SpriteX,_SpriteY,_spriteAncho,_spriteAlto)
 {
-    hojaSprites.load(":/Recursos/SpriteHomeroCaminando.png");
+    hojaSprites.load(QString::fromStdString(rutaSprite));
     sprite = hojaSprites.copy(SpriteX,SpriteY,spriteAncho,spriteAlto);
     setPixmap(sprite);
 }
@@ -13,12 +14,12 @@ Heroe::Heroe()
 void Heroe::keyPressEvent(QKeyEvent *event){
     switch (event->key()) {
     case Qt::Key_A:
-        movimiento(-7,0);
-        confSprite(160);
+        movimiento(-6,0);
+        secuenciaSprite(160,8);
         break;
     case Qt::Key_D:
-        movimiento(7,0);
-        confSprite(0);
+        movimiento(6,0);
+        secuenciaSprite(0,8);
         break;
     default:
         break;
@@ -35,19 +36,6 @@ void Heroe::movimiento(int dx, int dy)
         x=-38;
     }
     x += dx;
-    qDebug() << x;
     y += dy;
     setPos(x,y);
-}
-
-void Heroe::confSprite(int direccion)
-{
-    SpriteY = direccion;
-    SpriteX = spriteAncho*contador;
-    sprite = hojaSprites.copy(SpriteX,SpriteY,spriteAncho,spriteAlto);
-    setPixmap(sprite);
-    contador++;
-    if(contador==8){
-        contador = 0;
-    }
 }
