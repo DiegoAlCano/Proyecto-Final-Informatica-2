2d6d06416c37e55ba4a6b166fb553d956d1cc764
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-11-15 13:15:33 -0500

    Update Consumible.h

--- Consumible.h
+++ Consumible.h
@@ -10,11 +10,16 @@
 class Consumible : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
-public:
-    Consumible();
 
+public:
+    Consumible(const QString &rutaSprite, int _velocidadCaida, int _aumentoMunicion, int _aumentoVida);
+private:
+    int velocidadCaida;
+    int aumentoMunicion;
+    int aumentoVida;
 public slots:
     void caida();
 };
 
 #endif // CONSUMIBLE_H
+
