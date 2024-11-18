1b13e299616338697605c5593c9dc00dabbce325
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-11-18 01:40:47 -0500

    Update Consumible.cpp
    Se hace un aumento de 1 al score del héroe cuando alcanza un consumible

--- Consumible.cpp
+++ Consumible.cpp
@@ -1,8 +1,12 @@
 #include "Consumible.h"
 
-Consumible::Consumible()
+Consumible::Consumible(const QString &rutaSprite, int _velocidadCaida, int _aumentoMunicion, int _aumentoVida)
 {
-    setPixmap(QPixmap(":/Recursos/Dona.png")); // Cargar la imagen de la dona
+    velocidadCaida = _velocidadCaida;
+    aumentoMunicion = _aumentoMunicion;
+    aumentoVida = _aumentoVida;
+
+    setPixmap(QPixmap(rutaSprite)); // Cargar la imagen del consumible
 
     // Temporizador para el movimiento de la dona hacia abajo
     QTimer *fallTimer = new QTimer(this);
@@ -13,23 +17,29 @@ Consumible::Consumible()
 
 void Consumible::caida()
 {
-    setPos(x(), y() + 15);  // Mover la dona hacia abajo
+    setPos(x(), y() + velocidadCaida);  // Mover la dona hacia abajo
 
-    // Verificar colisiones con otros elementos en la escena
     QList<QGraphicsItem *> collidingItemsList = collidingItems();
     for (QGraphicsItem *item : collidingItemsList) {
         // Verificar si el elemento con el que colisiona es el héroe
-        if (dynamic_cast<Heroe *>(item)) {
-            // Eliminar la dona de la escena
+        Heroe *heroe = dynamic_cast<Heroe *>(item);
+        if (heroe) {
+            // Aumentar vida y munición del héroe
+            heroe->aumentarVida(aumentoVida);
+            heroe->aumentarMunicion(aumentoMunicion);
+            heroe->aumentarScore(1);
+
+            // Eliminar el consumible de la escena
             scene()->removeItem(this);
             delete this;
             return;
         }
     }
 
-    // Eliminar la dona si sale de la pantalla (suponiendo un límite de 800px de altura)
-    if (y() > 600) {
+    // Eliminar la dona si sale de la pantalla
+    if (y() > 620) {
         scene()->removeItem(this);
         delete this;
+        qDebug() <<"Dona eliminada";
     }
 }
