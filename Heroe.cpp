40300f5cc4db5165f8f60575ed88fcfc1659c60b
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-11-23 04:23:34 -0500

    Update Heroe.cpp

--- Heroe.cpp
+++ Heroe.cpp
@@ -34,11 +34,32 @@ void Heroe::keyPressEvent(QKeyEvent *event) {
         }
         break;
     case Qt::Key_W:
+
+        if(direccionHeroe==1){
+            secuenciaSprite(640,8);
+        }
+
+        else{
+            secuenciaSprite(800,8);
+        }
+
         if (!enElAire) {               // Inicia el salto solo si el héroe está en el suelo
             velocidadY = -15;           // Velocidad inicial hacia arriba
             velocidadX = 5*direccionHeroe;
             enElAire = true;
             saltoTimer->start(30);      // Inicia el temporizador para el salto
+            if(direccionHeroe==1){
+                secuenciaSprite(640,8);
+            }
+
+            else{
+                secuenciaSprite(800,8);
+            }
+        }
+        break;
+    case Qt::Key_P:
+        if(municion>0){
+            disparar(direccionHeroe);
         }
         break;
     default:
@@ -74,10 +95,20 @@ void Heroe::actualizarSalto() {
         enCaida = false;
         velocidadX = 0;  // Detiene el movimiento horizontal cuando aterriza
         saltoTimer->stop();
+
+        if(direccionHeroe==1){
+            secuenciaSprite(0,8);
+        }
+
+        else{
+            secuenciaSprite(160,8);
+        }
+
     }
 
     setPos(x, y);
-    qDebug() <<"X: "<< x<<"Y: "<< y;
+//    secuenciaSprite(640,8);
+//    qDebug() <<"X: "<< x<<"Y: "<< y;
 
 }
 
@@ -86,7 +117,9 @@ void Heroe::movimiento(int dx, int dy) {
 
     if (x > 555) {
         x = 555;
-    } else if (x < -55) {
+    }
+
+    else if (x < -55) {
         x = -55;
     }
     x += dx;
@@ -107,17 +140,36 @@ void Heroe::aumentarMunicion(int cantidadMunicion)
 {
     municion += cantidadMunicion;
 
+    if(municion>=12){
+        municion = 12;
+    }
+
 }
 
 void Heroe::disminuirVida(int cantidadVida)
 {
     vida -= cantidadVida;
 
+    if(vida<=0){
+        vida = 0;
+    }
+
+    if(direccionHeroe ==1){
+        secuenciaSprite(320,8);
+    }
+    else{
+        secuenciaSprite(480,8);
+    }
+
 }
 
 void Heroe::disminuirMunicion()
 {
     municion -= 1;
+
+    if(municion<=0){
+        municion = 0;
+    }
 }
 
 void Heroe::aumentarScore(int aumentoScore)
@@ -140,15 +192,36 @@ unsigned short Heroe::getScore()
     return score;
 }
 
-/*
+qreal Heroe::getPosX()
+{
+    return x;
+}
+
+
 void Heroe::disparar(int direccion)
 {
 
+    setPos(x,y);
+
+    if(direccion==1){
+        secuenciaSprite(960,8);
+    }
+
+    else{
+        secuenciaSprite(1120,8);
+    }
 }
-*/
+
+void Heroe::modificarPosX(qreal newX)
+{
+    x = newX;
+}
+
 qreal obtenerPosY(const Heroe &heroe) {
     return heroe.y;
 }
 
 bool obtenerenElAire(const Heroe &heroe){
+    return heroe.enElAire;
+}
     
