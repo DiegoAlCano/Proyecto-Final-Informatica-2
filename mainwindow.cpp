44dabeefec2a27e3426ab916d69c88689cafdeca
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-11-25 02:10:42 -0500

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,7 @@
 #include <time.h>
 #include <QPushButton>
 #include <QGraphicsProxyWidget>
+#include "Heroe.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -74,7 +75,7 @@ void MainWindow::iniciarNivel1()
 
     //Creacion de Personajes y consumibles
 //-----------------------------------------------------------------------------------------------------------------------
-    Heroe *Homero = new Heroe(0,0,150,160,100,0,":/Recursos/SpritesHomero.png");
+    Heroe *Homero = new Heroe('1',450,0,0,150,160,100,0,":/Recursos/SpritesHomero.png");
     escenaNivel1->addItem(Homero);
     Homero->setPos(0,450);
     Homero->setFlag(QGraphicsItem::ItemIsFocusable);
@@ -99,6 +100,7 @@ void MainWindow::iniciarNivel1()
     });
 
     donaTimer->start(1500);  // Crear una dona cada segundo
+    timers.append(donaTimer);
 
     //Creacion Enemigos
 
@@ -113,6 +115,7 @@ void MainWindow::iniciarNivel1()
     });
 
     enemigoTimer->start(2000);  // Crear un enemigo cada cierto tiempo
+    timers.append(enemigoTimer);
 
     QTimer *enemigoVoladorTimer = new QTimer(this);
     connect(enemigoVoladorTimer, &QTimer::timeout, this, [=]() {
@@ -123,6 +126,7 @@ void MainWindow::iniciarNivel1()
     });
 
     enemigoVoladorTimer->start(10000);
+    timers.append(enemigoVoladorTimer);
 
 //-----------------------------------------------------------------------------------------------------------------------
 
@@ -130,33 +134,13 @@ void MainWindow::iniciarNivel1()
 
 //-----------------------------------------------------------------------------------------------------------------------
     //Barras de Vida
-    QGraphicsRectItem *barraVidaRoja = new QGraphicsRectItem(0,0,100,20);
-    barraVidaRoja->setPos(50, 10); // Posición en la escena
-    barraVidaRoja->setBrush(QBrush(Qt::red));
-    escenaNivel1->addItem(barraVidaRoja);
+    QGraphicsRectItem *barraVidaRoja = nullptr;
+    QGraphicsRectItem *barraVidaAmarilla = nullptr;
+    añadirBarrasVida(escenaNivel1,50,10,barraVidaRoja,barraVidaAmarilla);
 
-    QGraphicsRectItem *barraVidaAmarilla = new QGraphicsRectItem();
-    barraVidaAmarilla->setPos(50, 10); // Posición en la escena
-    barraVidaAmarilla->setBrush(QBrush(Qt::yellow));
-    escenaNivel1->addItem(barraVidaAmarilla);
-
-    QPixmap Dona(":/Recursos/Dona.png");
-    QPixmap DonaRedimensionada = Dona.scaled(45,45,Qt::KeepAspectRatio);
-    QGraphicsPixmapItem *itemDona = new QGraphicsPixmapItem(DonaRedimensionada);
-    itemDona->setPos(320, -5); // Posición en la escena
-    escenaNivel1->addItem(itemDona);
-
-    QPixmap Corazon(":/Recursos/corazon.png");
-    QPixmap CorazonRedimensionado = Corazon.scaled(45,45,Qt::KeepAspectRatio);
-    QGraphicsPixmapItem *itemCorazon = new QGraphicsPixmapItem(CorazonRedimensionado);
-    itemCorazon->setPos(0, -5); // Posición en la escena
-    escenaNivel1->addItem(itemCorazon);
-
-    QPixmap Reloj(":/Recursos/Reloj.png");
-    QPixmap RelojRedimensionado = Reloj.scaled(45,45,Qt::KeepAspectRatio);
-    QGraphicsPixmapItem *itemReloj = new QGraphicsPixmapItem(RelojRedimensionado);
-    itemReloj->setPos(480, -5); // Posición en la escena
-    escenaNivel1->addItem(itemReloj);
+    añadirElementoGrafico(escenaNivel1,320,-5,":/Recursos/Dona.png");
+    añadirElementoGrafico(escenaNivel1,0,-5,":/Recursos/corazon.png");
+    añadirElementoGrafico(escenaNivel1,480,-5,":/Recursos/Reloj.png");
 //-----------------------------------------------------------------------------------------------------------------------
 
     //ELEMENTOS DE TEXTO
@@ -188,6 +172,7 @@ void MainWindow::iniciarNivel1()
         }
     });
     duracionNivel->start(1000);  // Actualizar cada segundo
+    timers.append(duracionNivel);
 
     //Vida Disponible
     QGraphicsTextItem *VidaText = new QGraphicsTextItem();
@@ -215,12 +200,7 @@ void MainWindow::iniciarNivel1()
         //Condicion de Derrota
         if (vidaActual <= 0) {
 
-            actualizarInfo->stop();
-            duracionNivel->stop();
-            donaTimer->stop();
-            enemigoTimer->stop();
-            actualizarInfo->stop();
-            enemigoVoladorTimer->stop();
+            detenerTimers();
 
             escenaNivel1->clear();
 
@@ -230,11 +210,7 @@ void MainWindow::iniciarNivel1()
         //Condicion de Victoria
         else if(donasComidas==100){
 
-            duracionNivel->stop();
-            donaTimer->stop();
-            enemigoTimer->stop();
-            actualizarInfo->stop();
-            enemigoVoladorTimer->stop();
+            detenerTimers();
 
             escenaNivel1->clear();
 
@@ -243,6 +219,7 @@ void MainWindow::iniciarNivel1()
         }
     });
     actualizarInfo->start(100);
+    timers.append(actualizarInfo);
 //-----------------------------------------------------------------------------------------------------------------------
 
 }
@@ -252,52 +229,92 @@ void MainWindow::iniciarNivel2()
 {
     quitarBotones();
 
-    QImage background(":/Recursos/FondoNivel2.png");
-    escena->setBackgroundBrush(QBrush(background));
-    escena->setSceneRect(0, 0, background.width(), background.height());
+    QGraphicsScene *escenaNivel2 = new QGraphicsScene(this);
 
-    vista->setScene(escena);
+    QImage background(":/Recursos/FondoNivel2.png");
+    escenaNivel2->setBackgroundBrush(QBrush(background));
+    escenaNivel2->setSceneRect(0, 0, background.width(), background.height());
+
+    vista->setScene(escenaNivel2);
     vista->resize(background.width()+20,background.height()+20);
 
-    Heroe *Bart = new Heroe(0,0,150,160,100,50,":/Recursos/SpritesBart.png");
-    escena->addItem(Bart);
-    Bart->setPos(0,450);
+    Heroe *Bart = new Heroe('2',130,0,0,150,160,100,15,":/Recursos/SpritesBart.png");
+    escenaNivel2->addItem(Bart);
+    Bart->setPos(100,130);
     Bart->setFlag(QGraphicsItem::ItemIsFocusable);
     Bart->setFocus();
-
-    //Creacion Donas
-
-    // Inicializar la semilla para posiciones aleatorias
-    std::srand(std::time(nullptr));
-
-    // Temporizador para crear donas
-    QTimer *donaTimer = new QTimer(this);
-    connect(donaTimer, &QTimer::timeout, this, [=]() {
-        Consumible *Piedras = new Consumible(":/Recursos/Piedras.png",10,15,10);
-
-        // Generar una posición aleatoria en el eje x (ajustando al ancho de la escena)
-        int randomX = std::rand() % 540;
-        Piedras->setPos(randomX, 0);        // Comienza en la parte superior de la escena
-
-        // Añadir la dona a la escena
-        escena->addItem(Piedras);
-    });
-
-    donaTimer->start(10000);
-
-    //Creacion Enemigos
 
     // Temporizador para crear enemigos desde los bordes
     QTimer *enemigoTimer = new QTimer(this);
     connect(enemigoTimer, &QTimer::timeout, this, [=]() {
         bool lado = (rand() % 2 == 0);
-        Enemigo *enemigo = new Enemigo(lado,0,0,130,130,15,6,":/Recursos/SpriteGremlin.png");
+        Enemigo *enemigo = new Enemigo(lado,0,0,130,130,20,13,130,550,135,10,":/Recursos/SpriteGremlin.png");
 
         // Añadir el enemigo a la escena
-        escena->addItem(enemigo);
+        escenaNivel2->addItem(enemigo);
+        connect(enemigo, &Enemigo::enemigoEliminado, Bart, &Heroe::aumentarScore);
     });
+    enemigoTimer->start(2000);
+    timers.append(enemigoTimer);
 
-    enemigoTimer->start(3000);
+    QGraphicsRectItem *barraVidaRoja = nullptr;
+    QGraphicsRectItem *barraVidaAmarilla = nullptr;
+    añadirBarrasVida(escenaNivel2,50,10,barraVidaRoja,barraVidaAmarilla);
+
+    añadirElementoGrafico(escenaNivel2, 0,-5,":/Recursos/corazon.png");
+    añadirElementoGrafico(escenaNivel2,300,0,":/Recursos/piedra.png");
+    añadirElementoGrafico(escenaNivel2,450,0,":/Recursos/ImagenGremlin.png");
+
+    QGraphicsTextItem *VidaText = new QGraphicsTextItem();
+    VidaText->setDefaultTextColor(Qt::white);
+    VidaText->setFont(QFont("Algerian", 20));
+    VidaText->setPos(150,0);
+    escenaNivel2->addItem(VidaText);
+
+    QGraphicsTextItem *MunicionText = new QGraphicsTextItem();
+    MunicionText->setDefaultTextColor(Qt::white);
+    MunicionText->setFont(QFont("Algerian", 20));
+    MunicionText->setPos(350,0);
+    escenaNivel2->addItem(MunicionText);
+
+    QGraphicsTextItem *ScoreActualText = new QGraphicsTextItem();
+    ScoreActualText->setDefaultTextColor(Qt::white);
+    ScoreActualText->setFont(QFont("Algerian", 20));
+    ScoreActualText->setPos(500,0);
+    escenaNivel2->addItem(ScoreActualText);
+
+    QTimer *actualizarInfo = new QTimer(this);
+    connect(actualizarInfo, &QTimer::timeout, this, [=](){
+        unsigned short int vidaActual = Bart->getVida();
+        unsigned short int municionActual = Bart->getMunicion();
+        unsigned short int scoreActual = Bart->getScore();
+
+        mostrarVida(barraVidaAmarilla,vidaActual);
+
+        VidaText->setPlainText(QString("%1/100").arg(vidaActual));
+        MunicionText->setPlainText(QString("x%1").arg(municionActual));
+        ScoreActualText->setPlainText(QString("x%1").arg(scoreActual));
+        //Condicion de Derrota
+        if(scoreActual == 12){
+
+            delete ScoreActualText;
+            delete Bart;
+            escenaNivel2->clear();
+            detenerTimers();
+
+            iniciarSegundaParteNivel2(escenaNivel2);
+        }
+
+        else if (vidaActual <= 0 or (municionActual == 0 and scoreActual<12)) {
+            detenerTimers();
+            escenaNivel2->clear();
+
+            mostrarGameOver(escenaNivel2);
+        }
+    });
+    actualizarInfo->start(100);
+    timers.append(actualizarInfo);
+
 }
 
 void MainWindow::iniciarNivel3()
@@ -319,7 +336,7 @@ void MainWindow::iniciarNivel3()
     vista->setScene(escenaNivel3);
     vista->resize(background.width() + 20, background.height() + 20);
 
-    Heroe *Homero = new Heroe(0, 0, 150, 160, 100, 12, ":/Recursos/SpritesHomero.png");
+    Heroe *Homero = new Heroe('1',450,0, 0, 150, 160, 100, 12, ":/Recursos/SpritesHomero.png");
     escenaNivel3->addItem(Homero);
     Homero->setPos(0, 450);
     Homero->setFlag(QGraphicsItem::ItemIsFocusable);
@@ -451,11 +468,124 @@ void MainWindow::mostrarVictoria(QGraphicsScene *escenaActual)
     });
 }
 
-void MainWindow::mostrarVida(QGraphicsRectItem *barraVidaAmarilla ,int vidaActual)
+void MainWindow::mostrarVida(QGraphicsRectItem *barraVidaAmarilla , int vidaActual)
 {
     short int vidaDisponible = 100-vidaActual;
 
     barraVidaAmarilla->setRect(100-vidaDisponible,0,vidaDisponible,20);
+}
+
+void MainWindow::iniciarSegundaParteNivel2(QGraphicsScene *escenaActual)
+{
+    Heroe *Bart = new Heroe('1',450,0,0,150,160,100,10,":/Recursos/SpritesBart.png");
+    escenaActual->addItem(Bart);
+    Bart->setPos(0,450);
+    Bart->setFlag(QGraphicsItem::ItemIsFocusable);
+    Bart->setFocus();
+
+    //Creacion Enemigos
+    Enemigo *enemigoMain = new Enemigo(300,0,130,130,130,30,27,":/Recursos/SpriteGremlin.png");
+        //(100,0,0,130,130,15,6,20,":/Recursos/SpriteGremlin.png");
+    escenaActual->addItem(enemigoMain);
+    enemigoMain->setPos(550,460);
+
+    // Temporizador para crear donas
+    QTimer *PiedrasTimer = new QTimer(this);
+    connect(PiedrasTimer, &QTimer::timeout, this, [=]() {
+        Consumible *Piedras = new Consumible(":/Recursos/Piedras.png",10,5,10);
+        int randomX = std::rand() % 540;
+        Piedras->setPos(randomX, 0);
+        escenaActual->addItem(Piedras);
+    });
+    PiedrasTimer->start(10000);
+    timers.append(PiedrasTimer);
+
+    QGraphicsRectItem *barraVidaRoja = nullptr;
+    QGraphicsRectItem *barraVidaAmarilla = nullptr;
+    añadirBarrasVida(escenaActual,50,10,barraVidaRoja,barraVidaAmarilla);
+
+    añadirElementoGrafico(escenaActual, 0,-5,":/Recursos/corazon.png");
+    añadirElementoGrafico(escenaActual,300,0,":/Recursos/piedra.png");
+    añadirElementoGrafico(escenaActual,450,0,":/Recursos/ImagenGremlin.png");
+
+    QGraphicsTextItem *VidaText = new QGraphicsTextItem();
+    VidaText->setDefaultTextColor(Qt::white);
+    VidaText->setFont(QFont("Algerian", 20));
+    VidaText->setPos(150,0);
+    escenaActual->addItem(VidaText);
+
+    QGraphicsTextItem *MunicionText = new QGraphicsTextItem();
+    MunicionText->setDefaultTextColor(Qt::white);
+    MunicionText->setFont(QFont("Algerian", 20));
+    MunicionText->setPos(350,0);
+    escenaActual->addItem(MunicionText);
+
+    QGraphicsTextItem *vidaEnemigoText = new QGraphicsTextItem();
+    vidaEnemigoText->setDefaultTextColor(Qt::white);
+    vidaEnemigoText->setFont(QFont("Algerian", 20));
+    vidaEnemigoText->setPos(500,0);
+    escenaActual->addItem(vidaEnemigoText);
+
+    QTimer *actualizarInfo = new QTimer(this);
+    connect(actualizarInfo, &QTimer::timeout, this, [=](){
+        unsigned short int vidaActual = Bart->getVida();
+        unsigned short int municionActual = Bart->getMunicion();
+        unsigned short int vidaEnemigo = enemigoMain->getVida();
+
+        mostrarVida(barraVidaAmarilla,vidaActual);
+
+        VidaText->setPlainText(QString("%1/100").arg(vidaActual));
+        MunicionText->setPlainText(QString("x%1").arg(municionActual));
+        vidaEnemigoText->setPlainText(QString("%1").arg(vidaEnemigo));
+        //Condicion de Derrota
+        if (vidaActual <= 0) {
+            escenaActual->clear();
+            detenerTimers();
+
+            mostrarGameOver(escenaActual);
+        }
+
+        else if(vidaEnemigo == 0){
+            escenaActual->clear();
+            detenerTimers();
+
+            mostrarVictoria(escenaActual);
+        }
+    });
+    actualizarInfo->start(100);
+    timers.append(actualizarInfo);
+}
+
+void MainWindow::añadirElementoGrafico(QGraphicsScene *escenaActual, int posX,int posY, const QString &rutaImagen)
+{
+    QPixmap imagen(rutaImagen);
+    QPixmap imagenRedimensionada = imagen.scaled(45,45,Qt::KeepAspectRatio);
+    QGraphicsPixmapItem *itemImagen = new QGraphicsPixmapItem(imagenRedimensionada);
+    itemImagen->setPos(posX, posY); // Posición en la escena
+    escenaActual->addItem(itemImagen);
+}
+
+void MainWindow::añadirBarrasVida(QGraphicsScene *escenaActual, int posX, int posY, QGraphicsRectItem *&barraVidaRoja, QGraphicsRectItem *&barraVidaAmarilla)
+{
+    barraVidaRoja = new QGraphicsRectItem(0,0,100,20);
+    barraVidaRoja->setPos(posX, posY); // Posición en la escena
+    barraVidaRoja->setBrush(QBrush(Qt::red));
+    escenaActual->addItem(barraVidaRoja);
+
+    barraVidaAmarilla = new QGraphicsRectItem();
+    barraVidaAmarilla->setPos(posX, posY); // Posición en la escena
+    barraVidaAmarilla->setBrush(QBrush(Qt::yellow));
+    escenaActual->addItem(barraVidaAmarilla);
+}
+
+void MainWindow::detenerTimers()
+{
+    for (QTimer *timer : timers) {
+
+        timer->stop();
+        delete timer;
+    }
+    timers.clear();
 }
 
 
@@ -468,4 +598,3 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
