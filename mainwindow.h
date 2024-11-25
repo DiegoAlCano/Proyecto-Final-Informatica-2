9f0f65fbae4e72bd2914c090244856aa551a9aa0
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-11-25 02:11:13 -0500

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,14 +1,17 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "Personaje.h"
 #include "Heroe.h"
 #include "Consumible.h"
 #include "Enemigo.h"
+#include "Proyectil.h"
 #include <QMainWindow>
 #include <QGraphicsView>
 #include <QGraphicsScene>
 #include <QGraphicsTextItem>
 #include <QPushButton>
+#include <QList>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -28,12 +31,17 @@ public:
     void mostrarGameOver(QGraphicsScene *escenaActual);
     void mostrarVictoria(QGraphicsScene *escenaActual);
     void mostrarVida(QGraphicsRectItem *barraVidaAmarilla, int vida);
+    void iniciarSegundaParteNivel2(QGraphicsScene *escenaActual);
+    void añadirElementoGrafico(QGraphicsScene *escenaActual, int posX, int posY, const QString &rutaImagen);
+    void añadirBarrasVida(QGraphicsScene *escenaActual, int posX, int posY, QGraphicsRectItem *&barraVidaRoja, QGraphicsRectItem *&barraVidaAmarilla);
+    void detenerTimers();
 
 private slots:
     void iniciarNivel1();
     void iniciarNivel2();
     void iniciarNivel3();
     void salirJuego();
+
 private:
     Ui::MainWindow *ui;
     QGraphicsView *vista;
@@ -47,6 +55,9 @@ private:
     //Para el nivel 3 - Control del flujo de las salas
     bool fondoCambiado1;
     bool fondoCambiado2;
+
+    QList<QTimer*> timers;
+
 
 };
 #endif // MAINWINDOW_H
