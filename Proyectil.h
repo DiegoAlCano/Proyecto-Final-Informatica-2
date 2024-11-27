391a53e303bef7f9c05da942b615d378c2ec025e
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-11-27 03:58:57 -0500

    Update Proyectil.h
    Se agregan dos sobre cargas al constructor de la clase Proyectil para proyectiles que se mueven en linea recta y en movimiento parabolico

--- Proyectil.h
+++ Proyectil.h
@@ -4,28 +4,36 @@
 #include <QGraphicsPixmapItem>
 #include <QObject>
 #include <QTimer>
+#include "Enemigo.h"
 
 class Proyectil : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 
 private:
+    //Variables para el movimiento
     double x;
     double y;
     double vx;
     double vy;
-    double gravedad;
-    bool der;
-    double velocidadHorizontal = 270.0;
+    double v0;
+    double tiempo=0;
+    double angulo;
+    short int direccion;
+
+    //Daño que causa el proyectil
+    short int daño;
+
     QTimer* timer;
 
 
 public:
-    Proyectil(double xInicial, double yInicial, bool direccionDerecha, QObject* parent = nullptr);
+    Proyectil(short int _daño, double _angulo, double _v0, double xInicial, double yInicial, short int _direccion);
+    Proyectil(short int _daño, double xInicial, double yInicial, short int _direccion);
 
 private slots:
-    void actualizar();
-signals:
+    void movimientoParabolico();
+    void movimientoRectilineo();
 };
 
 #endif // PROYECTIL_H
