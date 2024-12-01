874de79936d07887e1e9e4664c9c59dfe0db3349
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-12-01 11:43:25 -0500

    Update Consumible.h
    Cambios en los tipos de dato de los atributos de la clase

--- Consumible.h
+++ Consumible.h
@@ -12,14 +12,18 @@ class Consumible : public QObject, public QGraphicsPixmapItem
     Q_OBJECT
 
 public:
-    Consumible(const QString &rutaSprite, int _velocidadCaida, int _aumentoMunicion, int _aumentoVida);
+    Consumible(const QString &rutaSprite,unsigned short int _velocidadCaida,unsigned short int _aumentoMunicion,unsigned short int _aumentoVida);
+
+    void setAumentoVida(unsigned short int _aumentoVida);
+    void setaumentoMunicion(unsigned short int _aumentoMunicion);
 private:
-    int velocidadCaida;
-    int aumentoMunicion;
-    int aumentoVida;
+    unsigned short int velocidadCaida;
+    unsigned short int aumentoMunicion;
+    unsigned short int aumentoVida;
 public slots:
     void caida();
 };
 
 #endif // CONSUMIBLE_H
+
 
